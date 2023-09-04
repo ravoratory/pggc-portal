026061128caacea5bb5040244d1b6030dac2e46c
IzmYuta <y.izumi@4nonome.com>
2023-09-04 13:06:49 +0900

    Implement user model

---
+++ server/src/users/interfaces/update-user.input.ts
@@ -0,0 +1,20 @@
+import { Field, InputType } from "@nestjs/graphql";
+import { Role } from "@prisma/client";
+
+@InputType()
+export class UpdateUserInput {
+    @Field({ nullable: true })
+    pgritId: string;
+
+    @Field({ nullable: true })
+    githubId: string;
+
+    @Field({ nullable: false })
+    userId: string;
+
+    @Field({ nullable: false })
+    password: string;
+
+    @Field(() => Role, { nullable: false })
+    role: Role;
+}
--- server/src/users/users.resolver.ts
+++ server/src/users/users.resolver.ts
@@ -1,4 +1,64 @@
-import { Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
+import { PrismaService } from "src/prisma.service";
 
-@Resolver()
-export class UsersResolver {}
+import { CreateUserInput } from "./interfaces/create-user.input";
+import { UpdateUserInput } from "./interfaces/update-user.input";
+import { User } from "./interfaces/user.model";
+
+@Resolver(() => CreateUserInput)
+export class UsersResolver {
+    constructor(private readonly prismaService: PrismaService) { }
+
+    @Query(() => [User], { name: "users", nullable: true })
+    async getUsers() {
+        return this.prismaService.user.findMany();
+    }
+
+    @Query(() => User, { name: "user", nullable: true })
+    async getUser(
+        @Args({ name: "userId", type: () => String }) userId: string,
+    ) {
+        return this.prismaService.user.findFirst({
+            where: {
+                userid: userId,
+            },
+        });
+    }
+
+    @Mutation(() => User)
+    async createUser(@Args("input") input: CreateUserInput) {
+        return this.prismaService.user.create({
+            data: {
+                userid: input.userId,
+                pgritId: input.pgritId,
+                githubId: input.githubId,
+                password: input.password,
+            },
+        });
+    }
+
+    @Mutation(() => User)
+    async updateUser(@Args("input") input: UpdateUserInput) {
+        return this.prismaService.user.update({
+            where: {
+                userid: input.userId,
+            },
+            data: {
+                pgritId: input.pgritId,
+                githubId: input.githubId,
+                password: input.password,
+            },
+        });
+    }
+
+    @Mutation(() => User)
+    async deleteUser(
+        @Args({ name: "userId", type: () => String }) userId: string,
+    ) {
+        return this.prismaService.user.delete({
+            where: {
+                userid: userId,
+            },
+        });
+    }
+}
