70fa2a50e77ad8f1b2ae61f768b4b8e6360a82da
Kota <k.suzuki@4nonome.com>
2023-09-06 00:25:40 +0900

    Clarification

---
+++ server/src/clarifications/interfaces/update-clarification.input.ts
@@ -0,0 +1,24 @@
+import { Field, InputType } from "@nestjs/graphql";
+import { Problem } from "src/problems/interfaces/problem.model";
+import { Team } from "src/teams/interfaces/team.model";
+
+@InputType()
+export class UpdateClarificationInput {
+    @Field({ nullable: false })
+    id: number;
+
+    @Field({ nullable: false })
+    question: string;
+
+    @Field({ nullable: true })
+    answer?: string;
+
+    @Field(() => Team, { nullable: false })
+    team: Team;
+
+    @Field(() => Problem, { nullable: false })
+    problem: Problem;
+
+    @Field({ nullable: false })
+    isPublic: boolean;
+}
